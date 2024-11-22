ff5489eb8dbd32f317d3fb77c88bdccbf2aa70c5
karthi-88-hue <karthikeyan.mech24@gmail.com>
2024-11-22 17:04:26 +0530

    valid

---
+++ src/main/java/com/Dock/Controller/dockController.java
@@ -0,0 +1,11 @@
+package com.Dock.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class dockController {
+	public String dockmonk() {
+		return "all is well";
+	}
+
+}
